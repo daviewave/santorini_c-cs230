0c6d692393c512e6cfbb043aca039f25f2f60d37
ds <dav.silveira@proton.me>
2025-04-11 18:37:53 -0400

    reworked some typedefs and structs function logic for initalizing and printing the gameboard complete

--- src/game.h
+++ src/game.h
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define ROWS 7
+#define COLS 7
+
 typedef struct {
     int x;
     int y;
@@ -13,19 +16,30 @@ typedef struct {
 
 typedef struct {
     int level;
-    bool is_occupied;
+    char board_display;
+    char occupied; // will initialized as Null and replaced with either "A" or "P"
     Coordinate coordinate;
 } Space;
 
+//1. in the playable board each board_display will be = to its level (2)
+//2. every occupied will be ""
+//3. coordinate x will be = i while y = j
+//4. level = 2
+
 typedef struct {
-    char* name;
+    char name;
+    int score;
     Coordinate curr_space;
     Coordinate next_space;
-    int score;
 } Player;
 
-void initialize_gameboard(void);
-void print_board(void);
+typedef Space GameBoard[ROWS][COLS];
+
+
+GameBoard* initialize_gameboard(void);
+void print_board(GameBoard* board);
+void set_board_display(Space* space);
+char int_to_char(int n);
 void print_starting_msg(void);
 void prompt_space_selection(bool is_first);
 bool is_space_on_board(int x, int y);
--- src/main.c
+++ src/main.c
@@ -1,8 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "game.h"
 
+void set_board_display(Space* space){
+    // 
+}
+
+char int_to_char(int n){
+    return '0' + n;
+}
+
+GameBoard* initialize_gameboard() {
+    static Space board[ROWS][COLS];
+    
+    for(int i=0;i<ROWS;i++){
+        for(int j=0;j<COLS;j++){
+            if(i == 0 && j == 0){
+                board[i][j].board_display = ' ';
+            } else if(i == 0 && j != 0){ 
+                // label row
+                board[i][j].board_display = int_to_char(j);
+            } else if(j == 0 && i != 0){ 
+                // label column
+                board[i][j].board_display = int_to_char(i);
+            } else {
+                // playable space
+                board[i][j].coordinate.x = i;
+                board[i][j].coordinate.y = j;
+                board[i][j].level = 2;
+                board[i][j].board_display = '2'; // level to start since user hasnt been prompted for starting space
+            }
+        }
+    }
+    return &board;
+}
+
+void print_board(GameBoard *board) {
+    for(int i=0;i<ROWS;i++){
+        for(int j=0;j<COLS;j++){
+            printf("%c ", (*board)[i][j].board_display);
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char *argv[]){
+    GameBoard *board = initialize_gameboard();
+    print_board(board);
+
     return 0;
 }
