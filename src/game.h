0fe2ef91c44f485c1a2a0398ae4c3e14e9c5f52e
ds <dav.silveira@proton.me>
2025-04-11 22:57:48 -0400

    more updates to function definitions as i work my way through

--- src/game.h
+++ src/game.h
@@ -12,43 +12,49 @@
 typedef struct {
     int x;
     int y;
-} Coordinate;
+} Coordinates;
 
 typedef struct {
     int level;
     char board_display;
-    char occupied; // will initialized as Null and replaced with either "A" or "P"
-    Coordinate coordinate;
+    char occupied;
+    Coordinates coordinates;
 } Space;
-
-//1. in the playable board each board_display will be = to its level (2)
-//2. every occupied will be ""
-//3. coordinate x will be = i while y = j
-//4. level = 2
 
 typedef struct {
     char name;
     int score;
-    Coordinate curr_space;
-    Coordinate next_space;
+    Coordinates curr;
+    Coordinates next;
 } Player;
 
 typedef Space GameBoard[ROWS][COLS];
 
-
+// 1.
 GameBoard* initialize_gameboard(void);
 void print_board(GameBoard* board);
-void set_board_display(Space* space);
-char int_to_char(int n);
+
+// 2. 
+Player* add_player(char name);
 void print_starting_msg(void);
-void prompt_space_selection(bool is_first);
-bool is_space_on_board(int x, int y);
-bool is_straight_line(Coordinate curr, Coordinate next);
+Coordinates get_coordinates_input(void);
+Coordinates get_ai_starting_coordinates(Coordinates user_location);
+bool is_valid_coordinate(int coordinate);
+int* get_allowed_directions(Coordinates user_location);
+// bool allowed
+
+
+bool is_straight_line(Coordinates curr, Coordinates next);
 int get_random_coordinate(void);
-bool is_unobstructed_path(Coordinate *path, Coordinate opp_coords);
+bool is_unobstructed_path(Coordinates *path, Coordinates opp_coords);
 void update_level(Space space, bool is_ai);
 void update_score(Player player);
 void display_game_over_message(Player player);
 
+// helper functions
+char int_to_char(int n);
+void set_board_display(Space* space);
+int get_random_num(int min, int max);
+bool random_num_in_allowed(int target, int* arr);
 
 #endif
